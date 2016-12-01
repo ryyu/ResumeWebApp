bf9515ad81b4d9c7ef73b0468ec47ba38ba0064c
ryyu <yuryan45@gmail.com>
2016-11-30 17:55:29 -0800

    11/30

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var school = require('./routes/school_routes');
 var account = require('./routes/account_routes');
+var resume = require('./routes/resume_routes');
+var company = require('./routes/company_routes');
 
 var app = express();
 
@@ -27,6 +29,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/school', school);
 app.use('/account', account);
+app.use('/resume', resume);
+app.use('/company', company);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/account_routes.js
+++ routes/account_routes.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var account_dal = require('../model/account_dal.js');
+var account_dal = require('../model/account_dal');
 
 
 // View All Accounts
@@ -14,6 +14,25 @@ router.get('/all', function (req, res) {
             res.render('account/accountViewAll', {'result':result});
         }
     });
+});
+
+// View the account for the given id
+router.get('/', function(req, res)
+{
+    if(req.query.account_id == null) {
+        res.send('account_id is null');
+    }
+    else 
+    {
+       account_dal.getById(req.query.account_id, function(err,result) {
+            if (err) {
+                res.send(err);
+            }
+            else {
+                res.render('account/accountViewById', {'result': result});
+            }
+        });
+    }
 });
 
 module.exports = router;
