d44cf0c190fb76990f30a3d60cb0d5850bb49a1c
Unknown <unknown>
2016-12-07 17:54:38 -0800

    12/7

--- model/resume_dal.js
+++ model/resume_dal.js
@@ -6,7 +6,7 @@ var connection = mysql.createConnection(db.config);
 
 exports.getAll = function(callback)
 {
-    var query = 'SELECT * FROM resume;';
+    var query = 'SELECT * FROM resume_account_info;';
 
     connection.query(query, function(err, result) 
     {
@@ -24,7 +24,7 @@ exports.getById = function(resume_id, callback)
         callback(err, result);
     });
 };
-
+/*
 exports.insert = function(params, callback) {
     var query = 'INSERT INTO resume (resume_name, account_id) VALUES (?, ?)';
 
@@ -37,6 +37,69 @@ exports.insert = function(params, callback) {
     });
 
 };
+*/
+exports.insert = function(params, callback) {
+
+    // FIRST INSERT THE resume
+    var query = 'INSERT INTO resume (resume_name, account_id) VALUES (?, ?)';
+
+    var queryData = [params.resume_name, params.account_id];
+
+    // connection for multiple inserts
+    
+    connection.query(query, params.resume_name, function(err, result) {
+
+        // THEN USE THE COMPANY_ID RETURNED AS insertId AND THE SELECTED ADDRESS_IDs INTO COMPANY_ADDRESS
+        var resume_id = result.insertId;
+
+        // NOTE THAT THERE IS ONLY ONE QUESTION MARK IN VALUES ?
+        var query = 'INSERT INTO resume_skill (resume_id, skill_id) VALUES ?';
+
+        // TO BULK INSERT RECORDS WE CREATE A MULTIDIMENSIONAL ARRAY OF THE VALUES
+        var resumeSkillData = [];
+
+        if (resumeSkillData instanceof Array) {
+            for (var i = 0; i < params.skill_id.length; i++) {
+                resumeSkillData.push([resume_id, params.skill_id[i]]);
+            }
+        }
+        else
+        {
+            resumeSkillData.push([resume_id, params.skill_id[i]]);
+        }
+
+        // NOTE THE EXTRA [] AROUND companyAddressData
+        connection.query(query, [resumeSkillData], function(err, result){
+            //callback(err, result);
+        });
+    });
+  
+
+    /*
+
+    connection.query(query, params.resume_name, function(err, result) {
+
+        // THEN USE THE resume_ID RETURNED AS insertId AND THE SELECTED skills_IDs INTO resume_skill
+        var resume_id = result.insertId;
+
+        // NOTE THAT THERE IS ONLY ONE QUESTION MARK IN VALUES ?
+        var querySkill = 'INSERT INTO resume_skill (resume_id, skill_id) VALUES ?';
+
+        // TO BULK INSERT RECORDS WE CREATE A MULTIDIMENSIONAL ARRAY OF THE VALUES
+        var resumeSkillData = [];
+        for(var i=0; i < params.skill_id.length; i++) {
+            resumeSkillData.push([resume_id, params.skill_id[i]]);
+        }
+
+        // NOTE THE EXTRA [] AROUND resumeSkillData
+        connection.query(query, [resumeSkillData], function(err, result){
+            callback(err, result);
+        });
+    });
+    */
+
+};
+
 
 exports.delete = function(resume_id, callback) {
     var query = 'DELETE FROM resume WHERE resume_id = ?';
--- routes/school_routes.js
+++ routes/school_routes.js
@@ -47,7 +47,7 @@ router.get('/add', function(req, res){
     });
 });
 
-// insert a school record
+// View the school for the given id
 router.get('/insert', function(req, res){
     // simple validation
     if(req.query.school_name == null) {
@@ -60,14 +60,47 @@ router.get('/insert', function(req, res){
         // passing all the query parameters (req.query) to the insert function instead of each individually
         school_dal.insert(req.query, function(err,result) {
             if (err) {
+                console.log(err)
                 res.send(err);
             }
             else {
-                //poor practice, but we will handle it differently once we start using Ajax
+                //poor practice for redirecting the user to a different page, but we will handle it differently once we start using Ajax
                 res.redirect(302, '/school/all');
             }
         });
     }
+});
+
+router.get('/edit', function(req, res){
+    if(req.query.school_id == null) {
+        res.send('A school id is required');
+    }
+    else {
+        school_dal.edit(req.query.school_id, function(err, result){
+            res.render('school/schoolUpdate', {school: result[0][0], address: result[1]});
+        });
+    }
+
+});
+
+router.get('/edit2', function(req, res){
+   if(req.query.school_id == null) {
+       res.send('A school id is required');
+   }
+   else {
+       school_dal.getById(req.query.school_id, function(err, school){
+           address_dal.getAll(function(err, address) {
+               res.render('school/schoolUpdate', {school: school[0], address: address});
+           });
+       });
+   }
+
+});
+
+router.get('/update', function(req, res){
+    school_dal.update(req.query, function(err, result){
+       res.redirect(302, '/school/all');
+    });
 });
 
 // Delete a school for the given school_id
